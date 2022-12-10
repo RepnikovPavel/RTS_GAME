49bd245a6c87a75f53af0036e51d36e8cb88f451
Repnikov Pavel <prepnik@mail.ru>
2022-12-10 05:40:10 +0300

    1

---
+++ TestHUD.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TestHUD.generated.h"
+
+UCLASS(Abstract)
+class RTS_API UTestHUD : public UUserWidget
+{
+GENERATED_BODY()
+	
+	
+};
